c61f4a9fe0e4a6510aa315443926402bc9ea47c7
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2026-02-13 20:52:10 +0100

    Strip punctuation and other characters from keywords

--- helpers.py
+++ helpers.py
@@ -259,7 +259,8 @@ def get_caption_for_image_file(file_path) -> str:
 
 
 def get_keywords_from_caption(caption, stopwords) -> list[str]:
-    keywords_full = caption.split()
+    sanitized_caption = re.sub(FOLDER_FORBIDDEN_CHARS, "", caption)
+    keywords_full = sanitized_caption.split()
     keywords_no_stopwords = [k for k in keywords_full if k.lower() not in stopwords]
     keywords_unique = list(dict.fromkeys(keywords_no_stopwords)) 
 
